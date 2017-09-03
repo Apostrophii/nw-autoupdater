144954399e9ab52444f1c67f98b60521799a531a
Luke Travis <luke.travis@k3integrations.com>
2017-09-03 14:55:31 -0700

    Instantly resolve

--- Lib/utils.js
+++ Lib/utils.js
@@ -56,7 +56,8 @@ async function launch( runnerPath, argv, cwd, logPath ){
        });
 
       child.unref();
-      setTimeout( resolve, 500 );
+      // setTimeout( resolve, 50 );
+      resolve();
    });
 }
 
